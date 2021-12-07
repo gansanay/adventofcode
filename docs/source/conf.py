dc7750548cd111db1c0b1650f74e59245fca1da5
Guillaume Ansanay-Alex <gansanay@gmail.com>
2021-12-07 21:25:15 +0100

    doc: autoapi extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ author = "Guillaume Ansanay-Alex"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ["autoapi.extension"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
